f15cd2dd9d9af99c217c69fe6b72dc0b81069f75
Mark Howe <mvahowe@gmail.com>
2025-04-15 08:44:47 +0200

    Load data for parallel view

--- src/components/Parallel.jsx
+++ src/components/Parallel.jsx
@@ -1,3 +1,37 @@
-export default function Parallel({srcSpec}) {
-    return <div>{JSON.stringify(srcSpec)}</div>
+import {useState, useEffect, useContext} from 'react';
+import {getJson, getAndSetJson, debugContext} from "pithekos-lib";
+
+export default function Parallel() {
+
+    const [parallel, setParallel] = useState([]);
+    const [juxtas, setJuxtas] = useState({
+        MAT: [],
+        MRK: [],
+        LUK: [],
+        JHN: []
+    });
+    const {debugRef} = useContext(debugContext);
+
+    useEffect(
+        () => {
+            const getStuff = async () => {
+                let newJuxtas = juxtas;
+                for (const bookCode of ["MAT", "MRK", "LUK", "JHN"]) {
+                    let response = await getJson(`/burrito/ingredient/raw/git.door43.org/BurritoTruck/fr_juxta/?ipath=${bookCode}.json`, debugRef.current);
+                    if (response.ok) {
+                        newJuxtas[bookCode] = response.json;
+                    }
+                }
+                setJuxtas(newJuxtas);
+                getAndSetJson({
+                    url: "/burrito/ingredient/raw/git.door43.org/BurritoTruck/en_syn/?ipath=parallel.json",
+                    setter: setParallel
+                });
+            };
+            getStuff().then();
+        },
+        []
+    );
+
+        return <div>{parallel.length === 0 ? "loading" : "loaded"}</div>
 }
