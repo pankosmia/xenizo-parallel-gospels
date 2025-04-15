ac22f3d87ed8e0f14e276e422a95e7b345f74671
Mark Howe <mvahowe@gmail.com>
2025-04-15 18:21:48 +0200

    Multiple parallel views

--- src/components/Parallel.jsx
+++ src/components/Parallel.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect, useContext} from 'react';
 import {getJson, getAndSetJson, debugContext} from "pithekos-lib";
-import {Divider, Grid2} from "@mui/material";
+import {Button, Grid2} from "@mui/material";
 
 export default function Parallel() {
 
@@ -11,6 +11,7 @@ export default function Parallel() {
         LUK: {},
         JHN: {}
     });
+    const [contentView, setContentView] = useState("gloss");
     const {debugRef} = useContext(debugContext);
 
     useEffect(
@@ -54,65 +55,121 @@ export default function Parallel() {
     if (parallel.length === 0) {
         return <div>Loading...</div>;
     } else {
-        return <Grid2 container style={{fontSize: "x-small"}} >
-            {
-                parallel.map((line, n) => <>{
-                        ["MRK", "LUK", "MAT", "JHN"].map(
-                            bookCode => <Grid2 container key={`${bookCode}-${n}`} size={3} spacing={1} item>
-                                {line[bookCode]["chapter"] ?
-                                    <>
-                                        <Grid2 item size={12} display="flex"
-                                               justifyContent="center"
-                                               alignContent="center"
-                                               sx={{backgroundColor: "#DDD"}}
-                                        >
-                                            <b><i>{`${bookCode} ${line[bookCode]["chapter"]}.${line[bookCode]["fromVerse"]}${line[bookCode]["fromVerse"] !== line[bookCode]["toVerse"] ? "-" + line[bookCode]["toVerse"] : ""}`}</i></b>
-                                        </Grid2>
-                                        {
-                                            juxtaIndexes(line[bookCode]["fromJuxta"], line[bookCode]["toJuxta"])
-                                                .map(ji => <>
-                                                    <Grid2 item size={12} display="flex" alignContent="center" justifyContent="center">
-                                                        <b>{ji + 1}</b>
-                                                    </Grid2>
-                                                        {
-                                                            juxtas[bookCode].sentences &&
-                                                            juxtas[bookCode].sentences[ji] &&
+        return <>
+            <Button
+                sx={{ml: 2, position: "absolute", top: "75px", right: "50px"}}
+                color="primary"
+                size="large"
+                variant="contained"
+                aria-label={contentView}
+                onClick={
+                    () => {
+                        if (contentView === "gloss") {
+                            setContentView("source")
+                        } else if (contentView === "source") {
+                            setContentView("juxta")
+                        } else {
+                            setContentView("gloss")
+                        }
+                    }
+                }
+            >
+                {contentView}
+            </Button>
+            <Grid2 container style={{fontSize: "x-small"}}>
+                {
+                    parallel.map((line, n) => <>{
+                            ["MRK", "LUK", "MAT", "JHN"].map(
+                                bookCode => <Grid2 container key={`${bookCode}-${n}`} size={3} spacing={1} item  display="flex" flexDirection="row" alignContent="flex-start">
+                                    {line[bookCode]["chapter"] ?
+                                        <>
+                                            <Grid2 item size={12} display="flex"
+                                                   justifyContent="center"
+                                                   alignContent="center"
+                                                   sx={{backgroundColor: "#DDD"}}
+                                            >
+                                                <b><i>{`${bookCode} ${line[bookCode]["chapter"]}.${line[bookCode]["fromVerse"]}${line[bookCode]["fromVerse"] !== line[bookCode]["toVerse"] ? "-" + line[bookCode]["toVerse"] : ""}`}</i></b>
+                                            </Grid2>
+                                            {
+                                                juxtaIndexes(line[bookCode]["fromJuxta"], line[bookCode]["toJuxta"])
+                                                    .map(ji => <>
+                                                            {
+                                                                contentView === "juxta" &&
+                                                                <Grid2 item size={12} display="flex" alignContent="center"
+                                                                       justifyContent="center">
+                                                                    <b>{ji + 1}</b>
+                                                                </Grid2>
+                                                            }
+                                                            {
+                                                                contentView === "juxta" &&
+                                                                juxtas[bookCode].sentences &&
+                                                                juxtas[bookCode].sentences[ji] &&
+                                                                juxtas[bookCode].sentences[ji].chunks
+                                                                    .map(
+                                                                        (c, n) => <>
+                                                                            <Grid2
+                                                                                key={`${n}-2`}
+                                                                                item
+                                                                                size={6}
+                                                                                display="flex"
+                                                                                justifyContent="right"
+                                                                                alignItems="right"
+                                                                            >
+                                                                                {c.source.map(s => s.content).join(' ')}
+                                                                            </Grid2>
+                                                                            <Grid2
+                                                                                key={`${n}-3`}
+                                                                                item
+                                                                                size={6}
+                                                                            >
+                                                                                {c.gloss}
+                                                                            </Grid2>
+                                                                        </>
+                                                                    )
+                                                            }
+                                                        </>
+                                                    )
+                                            }
+                                            {
+                                                contentView === "source" &&
+                                                juxtaIndexes(line[bookCode]["fromJuxta"], line[bookCode]["toJuxta"])
+                                                    .map(ji => <Grid2 item size={12}>{
+                                                            `${ji}: ` +
                                                             juxtas[bookCode].sentences[ji].chunks
                                                                 .map(
-                                                                    (c, n) => <>
-                                                                        <Grid2
-                                                                            key={`${n}-2`}
-                                                                            display="flex"
-                                                                            item
-                                                                            size={6}
-                                                                            justifyContent="right"
-                                                                            alignItems="right"
-                                                                        >
-                                                                            {c.source.map(s => s.content).join(' ')}
-                                                                        </Grid2>
-                                                                        <Grid2
-                                                                            key={`${n}-3`}
-                                                                            item
-                                                                            size={6}
-                                                                        >
-                                                                            {c.gloss}
-                                                                        </Grid2>
-                                                                    </>
+                                                                    (c, n) => c.source.map(s => s.content).join(" ")
                                                                 )
+                                                                .join(" ") + "."
                                                         }
-                                                    </>
-                                                )
-                                        }
-                                    </> :
-                                    <Grid2 item size={12} display="flex" alignContent="center" justifyContent="center">---</Grid2>
-                                }
-                            </Grid2>
-                        )
-                    }
-                    </>
-                )
-            }
-        </Grid2>
+                                                        </Grid2>
+                                                    )
+                                            }
+                                            {
+                                                contentView === "gloss" &&
+                                                juxtaIndexes(line[bookCode]["fromJuxta"], line[bookCode]["toJuxta"])
+                                                    .map(ji => <Grid2 item size={12}>{
+                                                            `${ji}: ` +
+                                                            juxtas[bookCode].sentences[ji].chunks
+                                                                .map(
+                                                                    (c, n) => c.gloss
+                                                                )
+                                                                .join(" ")
+                                                        }
+                                                        </Grid2>
+                                                    )
+                                            }
+                                        </> :
+                                        <Grid2 item size={12} display="flex" alignContent="center"
+                                               justifyContent="center">---</Grid2>
+                                    }
+                                </Grid2>
+                            )
+                        }
+                        </>
+                    )
+                }
+            </Grid2>
+        </>
     }
 }
 
