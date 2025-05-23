13f01c64aebe1476c56386227b00c5446790772c
Mark Howe <mvahowe@gmail.com>
2025-05-23 12:23:25 +0200

    Display book intros

--- src/components/BookContent.jsx
+++ src/components/BookContent.jsx
@@ -1,5 +1,34 @@
-import {Typography} from "@mui/material";
+import {useState, useEffect} from 'react';
+import Markdown from "react-markdown";
+import RequireResources from "./RequireResources";
+import {getText} from "pithekos-lib";
 
 export default function BookContent({sectionPointer}) {
-    return <Typography>{`BOOK Content for ${sectionPointer.join(', ')}`}</Typography>
+    const [intros, setIntros] = useState({});
+
+    useEffect(() => {
+        const getIntros = async bookCodes => {
+        const newIntros = {};
+        for (const bookCode of bookCodes) {
+            const response = await getText(`/burrito/ingredient/raw/git.door43.org/BurritoTruck/fr_xenizo_book-notes?ipath=${bookCode}.md`);
+            if (response.ok) {
+                newIntros[bookCode] = response.text;
+            } else {
+                console.log(`Could not load markdown text for ${bookCode}: ${response.error}`);
+            }
+        }
+        setIntros(newIntros);
+    };
+    getIntros(["MAT", "MRK", "LUK", "JHN"]).then();
+},
+[]
+);
+
+    return <RequireResources
+        required={[
+            ["Introductions de Livre Xenizo (ILX)", "git.door43.org/BurritoTruck/fr_xenizo_book-notes",],
+        ]}
+    >
+        {intros[sectionPointer[0]] && <Markdown>{intros[sectionPointer[0]]}</Markdown>}
+    </RequireResources>
 }
