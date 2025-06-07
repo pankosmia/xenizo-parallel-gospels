10bd3c56d871d4f7ff0444d23a288ccdbd0dfb4b
Mark Howe <mvahowe@gmail.com>
2025-06-07 14:18:05 +0200

    Progressive tW notes

--- src/components/TwoPartNotes.jsx
+++ src/components/TwoPartNotes.jsx
@@ -1,21 +1,23 @@
 import {useState} from "react";
-import {Box, Button} from "@mui/material";
+import {Box, Button, Tooltip} from "@mui/material";
 import Markdown from "react-markdown";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 
-export default function({shortNote, longNote}) {
+export default function ({shortNote, longNote}) {
     const [showLong, setShowLong] = useState(false);
     return <Box>
-        <Button
-        onClick={() => setShowLong(!showLong)}
-        variant="text"
-        color="secondary"
-        endIcon={showLong ? <ExpandLess/> : <ExpandMore/>}
-        >
-            {shortNote}
-        </Button>
-        {   showLong &&
+        <Tooltip title={shortNote}>
+            <Button
+                onClick={() => setShowLong(!showLong)}
+                variant="text"
+                color="secondary"
+                endIcon={showLong ? <ExpandLess/> : <ExpandMore/>}
+            >
+                {shortNote.split(":")[0]}
+            </Button>
+        </Tooltip>
+        {showLong &&
             <Markdown>{longNote}</Markdown>
         }
     </Box>
